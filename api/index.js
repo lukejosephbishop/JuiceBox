7d7a7e40207ca5822ed7a185542b0a88b780cee7
Luke <reboluketion42@gmail.com>
2021-10-09 11:13:39 -0700

    finished part 2: part 2

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const apiRouter = express.Router();
 // Before we start attaching our routers
 
 const jwt = require('jsonwebtoken');
-const { getUserById } = require('../db');
+const { getUserById, getUserByUsername, createUser } = require('../db');
 const { JWT_SECRET } = process.env;
 
 // set `req.user` if possible
@@ -36,16 +36,65 @@ apiRouter.use(async (req, res, next) => {
   }
 });
 
+apiRouter.use((req, res, next) => {
+    if (req.user) {
+      console.log("User is set:", req.user);
+    }
+  
+    next();
+  });
+  
+
 // Attach routers below here
 
 
 const usersRouter = require('./users');
 apiRouter.use('/users', usersRouter);
 
+usersRouter.post('/register', async (req, res, next) => {
+    const { username, password, name, location } = req.body;
+  
+    try {
+      const _user = await getUserByUsername(username);
+  
+      if (_user) {
+        next({
+          name: 'UserExistsError',
+          message: 'A user by that username already exists'
+        });
+      }
+  
+      const user = await createUser({
+        username,
+        password,
+        name,
+        location,
+      });
+  
+      const token = jwt.sign({ 
+        id: user.id, 
+        username
+      }, process.env.JWT_SECRET, {
+        expiresIn: '1w'
+      });
+  
+      res.send({ 
+        message: "thank you for signing up",
+        token 
+      });
+    } catch ({ name, message }) {
+      next({ name, message })
+    } 
+  });
+
 const postsRouter = require('./posts');
 apiRouter.use('/posts', postsRouter);
 
 const tagsRouter = require('./tags');
 apiRouter.use('/tags', tagsRouter);
+
+apiRouter.use((error, req, res, next) => {
+    res.send(error);
+  });
 
 module.exports = apiRouter;
--- api/users.js
+++ api/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const usersRouter = express.Router();
-const { getAllUsers } = require("../db");
+const { getAllUsers, getUserByUsername } = require("../db");
+const jwt = require(`jsonwebtoken`);
 
 usersRouter.use((req, res, next) => {
   console.log("A request is being made to /users");
@@ -8,13 +9,52 @@ usersRouter.use((req, res, next) => {
   next();
 });
 
+usersRouter.get("/", async (req, res) => {
+  const users = await getAllUsers();
 
-usersRouter.get('/', async (req, res) => {
-    const users = await getAllUsers();
-  
-    res.send({
-      users
-    });
+  res.send({
+    users,
   });
+});
+
+usersRouter.post("/login", async (req, res, next) => {
+  const { username, password } = req.body;
+
+  // request must have both
+  if (!username || !password) {
+    next({
+      name: "MissingCredentialsError",
+      message: "Please supply both a username and password",
+    });
+  }
+
+  try {
+    const user = await getUserByUsername(username);
+
+    if (user && user.password == password) {
+      // create token & return to user
+      const token = jwt.sign(
+        {
+          id: user.id,
+          username: username,
+        },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "1w",
+        }
+      );
+
+      res.send({ message: "you're logged in!", token });
+    } else {
+      next({
+        name: "IncorrectCredentialsError",
+        message: "Username or password is incorrect",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
 
 module.exports = usersRouter;
