afba6c07dde6de1b65bf6dad710f5a224ef5b2d5
Luke <reboluketion42@gmail.com>
2021-10-08 22:24:20 -0700

    Finished Working inside of api/index module

--- api/index.js
+++ api/index.js
@@ -1,6 +1,44 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+// api/index.js
+// Before we start attaching our routers
+
+const jwt = require('jsonwebtoken');
+const { getUserById } = require('../db');
+const { JWT_SECRET } = process.env;
+
+// set `req.user` if possible
+apiRouter.use(async (req, res, next) => {
+  const prefix = 'Bearer ';
+  const auth = req.header('Authorization');
+
+  if (!auth) { // nothing to see here
+    next();
+  } else if (auth.startsWith(prefix)) {
+    const token = auth.slice(prefix.length);
+
+    try {
+      const { id } = jwt.verify(token, JWT_SECRET);
+
+      if (id) {
+        req.user = await getUserById(id);
+        next();
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  } else {
+    next({
+      name: 'AuthorizationHeaderError',
+      message: `Authorization token must start with ${ prefix }`
+    });
+  }
+});
+
+// Attach routers below here
+
+
 const usersRouter = require('./users');
 apiRouter.use('/users', usersRouter);
 
