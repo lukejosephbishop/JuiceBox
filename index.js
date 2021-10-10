b32544a697e49feb2deb958bc03765291f3a8f2d
Luke <reboluketion42@gmail.com>
2021-10-10 15:27:49 -0700

    semi-final push

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ console.log(process.env.JWT_SECRET);
 
 // EVERYTHING ELSE
 
-const PORT = 3000;
+const { PORT = 3000 } = process.env
 const express = require("express");
 
 const server = express();
