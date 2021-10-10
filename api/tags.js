ffb587acd61eab6ef58fa572ea06dd891d474e04
Luke <reboluketion42@gmail.com>
2021-10-10 15:40:51 -0700

    final commit

--- api/tags.js
+++ api/tags.js
@@ -1,39 +1,31 @@
 const express = require("express");
 const tagsRouter = express.Router();
-const { getAllTags, getPostsByTagName } = require("../db");
 
 tagsRouter.use((req, res, next) => {
-    console.log("A request is being made to /tags");
-  
-    next();
-  });
+  console.log("A request is being made to /tags");
 
-  tagsRouter.get('/', async (req, res) => {
-    const tags = await getAllTags();
-  
-    res.send({
-      tags
+  next();
+});
+
+const { getAllTags, getPostsByTagName } = require("../db");
+
+tagsRouter.get("/:tagName/posts", async (req, res, next) => {
+  const tags = await getAllTags();
+  const { tagName } = req.params;
+  try {
+    const allPosts = await getPostsByTagName(tagName);
+    // use our method to get posts by tag name from the db
+    // send out an object to the client { posts: // the posts }
+    const posts = allPosts.filter((post) => {
+      return post.active || (req.user && post.author.id === req.user.id);
     });
-  });
-
-  tagsRouter.get('/api/tags/:tagName/posts', async (req, res, next) => {
-    const tags = await getAllTags();
-    const { tagName } = req.params;
-    try {
-      const allPosts = await getPostsByTagName(tagName);
-      // use our method to get posts by tag name from the db
-      // send out an object to the client { posts: // the posts }
-      const posts = allPosts.filter((post) => {
-        return post.active || (req.user && post.author.id === req.user.id);
-      });
-      res.send({
-        posts,
-      });
-      
-    } catch ({ name, message }) {
-      // forward the name and message to the error handler
-      console.log({name, message})
-    }
-  });
+    res.send({
+      posts,
+    });
+  } catch ({ name, message }) {
+    // forward the name and message to the error handler
+    console.log({ name, message });
+  }
+});
 
 module.exports = tagsRouter;
